8c4eee301d5391787e4d037c5086d565d3f3be08
tanishq14developer <tanishq.happy@gmail.com>
2023-08-21 20:37:49 +0530

    Feat:code optimised

--- src/modules/rewards/rewards.controller.ts
+++ src/modules/rewards/rewards.controller.ts
@@ -2,11 +2,12 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req, Quer
 import { RewardsService } from './rewards.service';
 import { CreateRewardDto } from './dto/create-reward.dto';
 import { UpdateRewardDto } from './dto/update-reward.dto';
-import { ApiBearerAuth } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { JwtAuthGuard } from 'modules/auth/jwt-auth.guard';
 import { CommonRequestUser } from 'shared/dto/common-request-user.interface';
 
 @Controller('rewards')
+@ApiTags('rewards')
 export class RewardsController {
     constructor(private readonly rewardsService: RewardsService) { }
 
@@ -20,18 +21,18 @@ export class RewardsController {
         return this.rewardsService.getUserRewards(request);
     }
 
-    @Get(':id')
-    findOne(@Param('id') id: string) {
-        return this.rewardsService.findOne(+id);
-    }
-
-    @Patch(':id')
-    update(@Param('id') id: string, @Body() updateRewardDto: UpdateRewardDto) {
-        return this.rewardsService.update(+id, updateRewardDto);
-    }
-
-    @Delete(':id')
-    remove(@Param('id') id: string) {
-        return this.rewardsService.remove(+id);
-    }
+    /*  @Get(':id')
+     findOne(@Param('id') id: string) {
+         return this.rewardsService.findOne(+id);
+     }
+ 
+     @Patch(':id')
+     update(@Param('id') id: string, @Body() updateRewardDto: UpdateRewardDto) {
+         return this.rewardsService.update(+id, updateRewardDto);
+     }
+ 
+     @Delete(':id')
+     remove(@Param('id') id: string) {
+         return this.rewardsService.remove(+id);
+     } */
 }
